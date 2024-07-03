5dedf42d6839743393a351f222974b5c6f6e137d
Alex Tetervak <tetervak@sheridancollege.ca>
2024-07-03 11:26:57 -0400

    made some functions private

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,11 +18,11 @@ export class AppComponent {
     this.rollData = this.getRollData(3);
   }
 
-  getRandomDiceValue(): number{
+  private getRandomDiceValue(): number{
   return 1 + Math.floor(6 * Math.random());
   }
 
-  getRollData(numberOfDice: number): RollData {
+  private getRollData(numberOfDice: number): RollData {
     const values: number[] = [];
     let total: number = 0;
     for (let i: number = 0; i < numberOfDice; i++) {
